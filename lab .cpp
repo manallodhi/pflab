9b4dc6790e2012aa56f96d632215ed1337a89f68
manallodhi <lodhimanal8@gmail.com>
2025-01-08 23:46:29 +0500

    Done

---
+++ lab .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num1 , num2;
+  int sum = 0 ;
+  cout << "Enter the number : " ;
+  cin >> num1 ;
+  cout << "Enter the number : " ;
+  cin >> num2 ;
+
+  sum = num1 + num2 ;
+  cout << "Sum of " << num1 << " and " << num2 << " = " << sum ;
+
+  return 0 ;
+}
